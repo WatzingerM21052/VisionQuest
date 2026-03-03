89945d4c3e8364f77bf17b6c6e09a6db9aec28bf
WatzingerM21052 <watzingerm21052@sus.htl-grieskirchen.at>
2026-03-03 09:51:04 +0100

    Routing anpassen

---
+++ Project/frontend/lib/app_routes.dart
@@ -0,0 +1,9 @@
+class AppRoutes {
+  static const login = '/login';
+  static const register = '/register';
+  static const home = '/home';
+  static const scanner = '/scanner';
+  static const reward = '/reward';
+  static const questLog = '/quest-log';
+  static const settings = '/settings';
+}
